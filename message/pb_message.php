00004b0e494f8e305bf490337fabd26fa0e744bd
kordulla <kordulla@d0695d13-3852-0410-878a-3b8354214890>
2008-07-17 18:46:05 +0000

    Inital project setup for the protocol buffer for php project.

---
+++ message/pb_message.php
@@ -0,0 +1,288 @@
+<?
+/**
+ * Including of all files needed to parse messages
+ * @author Nikolai Kordulla
+ */
+require_once(dirname(__FILE__). '/' . 'encoding/pb_base128.php');
+require_once(dirname(__FILE__). '/' . 'type/pb_enum.php');
+require_once(dirname(__FILE__). '/' . 'type/pb_string.php');
+require_once(dirname(__FILE__). '/' . 'type/pb_int.php');
+/**
+ * Abstract Message class
+ * @author Nikolai Kordulla
+ */
+abstract class PBMessage
+{
+    const WIRED_VARINT = 0;
+    const WIRED_64BIT = 1;
+    const WIRED_STRING = 2;
+    const WIRED_START_GROUP = 3;
+    const WIRED_END_GROUP = 4;
+
+    var $base128;
+
+    // here are the field types
+    var $fields = array();
+    // the values for the fields
+    var $values = array();
+
+    // type of the class
+    var $wired_type = 2;
+
+    // the value of a class
+    var $value = null;
+
+    // modus byte or string parse (byte for productive string for better reading and debuging)
+    // 1 = byte, 2 = String
+    const MODUS = 1;
+
+    /**
+     * Constructor - initialize base128 class
+     */
+    public function __construct()
+    {
+        $this->value = $this;
+        $this->base128 = new base128varint(PBMessage::MODUS);
+    }
+
+    /**
+     * Get the wired_type and field_type
+     * @param $number as decimal
+     * @return array wired_type, field_type
+     */
+    public function get_types($number)
+    {
+        $binstring = decbin($number);
+        $types = array();
+        $low = substr($binstring, strlen($binstring) - 3, strlen($binstring));
+        $high = substr($binstring,0, strlen($binstring) - 3) . '0000';
+        $types['wired'] = bindec($low);
+        $types['field'] = bindec($binstring) >> 3;
+        return $types;
+    }
+
+    /**
+     * Just built packages and make hex to bin
+     * @param the message string
+     */
+    private function built_packages($message)
+    {
+        if (PBMessage::MODUS == 1)
+        {
+            $newmessage = '';
+            // now convert to hex
+            $newpart = '';
+            for ($i = 0; $i < mb_strlen($message); ++$i)
+            {
+                $newpart = dechex(ord($message[$i]));
+                if (strlen($newpart) % 2 == 1)
+                    $newpart = '0' . $newpart;
+                $newstring .= $newpart;
+            }
+            $message = $newstring;
+        }
+
+        $ret = array();
+        $package = '';
+        for ($i = 0; $i < mb_strlen($message); $i=$i+2)
+        {
+            $value = decbin(hexdec(substr($message,$i, 2)));
+
+            if (strlen($value) == 8)
+            {
+                // now fill to eight with 00
+                $package .= $value;
+            }
+            else
+            {
+                // now fill to length of eight with 0
+                $value = substr('00000000', 0, 8 - strlen($value) % 8) . $value;
+                $package .= $value;
+                $ret[] = $package;
+                $package = '';
+            }
+        }
+        return $ret;
+    }
+
+    /**
+     * Encodes a Message
+     * @return string the encoded message
+     */
+    public function SerializeToString($rec=-1)
+    {
+        $string = '';
+        // wired and type
+        if ($rec > -1)
+        {
+            $string .= $this->base128->set_value($rec << 3 | $this->wired_type);
+        }
+
+
+        $stringinner = '';
+
+        foreach ($this->fields as $index => $field)
+        {
+            if (is_array($this->values[$index]) && count($this->values[$index]) > 0)
+            {
+                // make serialization for every array
+                foreach ($this->values[$index] as $array)
+                {
+                    $newstring = '';
+                    $newstring .= $array->SerializeToString($index);
+
+                    $stringinner .= $newstring;
+                }
+            }
+            else if ($this->values[$index] != null)
+            {
+                // wired and type
+                $newstring = '';
+                $newstring .= $this->values[$index]->SerializeToString($index);
+
+                $stringinner .= $newstring;
+            }
+        }
+
+        if ($this->wired_type == PBMessage::WIRED_STRING && $rec > -1)
+        {
+            $stringinner = $this->base128->set_value(mb_strlen($stringinner) / PBMessage::MODUS) . $stringinner;
+        }
+
+        return $string . $stringinner;
+    }
+
+    /**
+     * Decodes a Message and Built its things
+     *
+     * @param message as stream of hex example '1a 03 08 96 01'
+     */
+    public function ParseFromString($message)
+    {
+        if (PBMessage::MODUS != 1)
+        {
+            $message = str_replace(' ','', $message);
+        }
+        //$type = 1;
+        $array = $this->built_packages($message);
+        // no messtype set
+        $messtypes = array();
+        $length = 0;
+
+        $this->_ParseFromArray($array);
+    }
+
+    /**
+     * Internal function
+     */
+    public function ParseFromArray(&$array)
+    {
+        // first byte is length
+        $length = $this->base128->get_value($array[0]);
+        $array = array_slice($array, 1, count($array));
+        $newlength  = 0;
+        $newarray = array();
+        while ($newlength < $length && !empty($array))
+        {
+            $newlength += strlen($array[0]) / 8;
+            $newarray[] = $array[0];
+            $array = array_slice($array, 1, count($array));
+        }
+        $this->_ParseFromArray($newarray);
+        // @TODO make typecheck this means required or not perhaps leave it out
+        return $this;
+    }
+
+    /**
+     * Internal function
+     */
+    private function _ParseFromArray(&$array)
+    {
+        while (!empty($array))
+        {
+            // number from base128
+            $number = $this->base128->get_value($array[0]);
+            $array = array_slice($array, 1, count($array));
+
+            // now get the message type
+            $messtypes = $this->get_types($number);
+
+            // now make method test
+            if (!isset($this->fields[$messtypes['field']]))
+            {
+            	// field is unknown so just ignore it
+                // throw new Exception('Field ' . $messtypes['field'] . ' not present ');
+                if ($messtypes['wired'] == PBMessage::WIRED_STRING)
+                	$consume = new PBString();
+                else if ($messtypes['wired'] == PBMessage::WIRED_VARINT)
+                	$consume = new PBInt();
+                // perhaps send a warning out                
+                $consume->parseFromArray($array);
+                continue;
+            }
+
+            // now array or not
+            if (is_array($this->values[$messtypes['field']]))
+            {
+                $this->values[$messtypes['field']][] = new $this->fields[$messtypes['field']]();
+                $index = count($this->values[$messtypes['field']]) - 1;
+                if ($messtypes['wired'] != $this->values[$messtypes['field']][$index]->wired_type)
+                    throw new Exception('Expected type:' . $messtypes['wired'] . ' but had ' . $this->fields[$messtypes['field']]->wired_type);
+                $this->values[$messtypes['field']][$index]->ParseFromArray($array);
+            }
+            else
+            {
+                $this->values[$messtypes['field']] = new $this->fields[$messtypes['field']]();
+                if ($messtypes['wired'] != $this->values[$messtypes['field']]->wired_type)
+                    throw new Exception('Expected type:' . $messtypes['wired'] . ' but had ' . $this->fields[$messtypes['field']]->wired_type);
+                $this->values[$messtypes['field']]->ParseFromArray($array);
+            }
+
+        }
+    }
+
+    /**
+     * Add an array value
+     * @param int - index of the field
+     */
+    protected function _add_arr_value($index)
+    {
+        return $this->values[$index][] = new $this->fields[$index]();
+    }
+
+    /**
+     * Set an value
+     * @param int - index of the field
+     * @param Mixed value
+     */
+    protected function _set_value($index, $value)
+    {
+        if (gettype($value) == 'object')
+        {
+            $this->values[$index] = $value;
+        }
+        else
+        {
+            $this->values[$index] = new $this->fields[$index]();
+            $this->values[$index]->value = $value;
+        }
+    }
+
+    protected function _get_value($index)
+    {
+        if ($this->values[$index] == null)
+            return null;
+        return $this->values[$index]->value;
+    }
+
+    protected function _get_arr_value($index, $value)
+    {
+        return $this->values[$index][$value];
+    }
+
+    protected function _get_arr_size($index)
+    {
+        return count($this->values[$index]);
+    }
+}
+?>
---
+++ message/type/pb_string.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @author Nikolai Kordulla
+ */
+class PBString extends PBMessage
+{
+    var $wired_type = PBMessage::WIRED_STRING;
+
+    /**
+     * Parses the message for this type
+     *
+     * @param array
+     */
+    public function ParseFromArray(&$array)
+    {
+        $string = '';
+        // first byte is length
+        $length = $this->base128->get_value($array[0]);
+        $array = array_slice($array, 1, count($array));
+
+        // now calculate the length
+        $newlength = 0;
+        for ($i=0; $i < $length; ++$i)
+        {
+            $newlength += strlen($array[$i]) / 8;
+            if ($newlength >= $length)
+                break;
+        }
+
+        if ($newlength < $length)
+        {
+            var_dump($array);
+            throw new Exception('Length is set to ' . $length . ' but ' . $newlength . ' available');
+        }
+
+        for ($i=0; $i < $newlength; ++$i)
+        {
+            $number = $this->base128->get_value($array[0]);
+            $array = array_slice($array, 1, count($array));
+            $string .= (chr($number));
+        }
+
+        $this->value = ($string);
+        return $string;
+    }
+
+    /**
+     * Serializes type
+     */
+   public function SerializeToString($rec = -1)
+   {
+        $string = '';
+
+        if ($rec > -1)
+            $string .= $this->base128->set_value($rec << 3 | $this->wired_type);
+
+        // now the string
+        $value = ($this->value);
+
+        $string .= $this->base128->set_value(mb_strlen($value));
+
+        for ($i=0; $i < strlen($value); ++$i)
+            $string .= $this->base128->set_value(ord($value[$i]));
+        return $string;
+   }
+}
+?>
